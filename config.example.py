2a40cef278c037e5704f1a843d554fcf19ffae54
Bluewy <bluewyechache@gmail.com>
2021-02-01 19:45:51 +0100

    Create config.example.py

---
+++ config.example.py
@@ -0,0 +1,12 @@
+token = 'YOUR_BOT_TOKEN'
+
+extensions = ['info.py',
+              'jishaku'
+]
+
+red = (7f0000)
+green = (2aa72a)
+orange = (FF7F50)
+blue = (00BFFF)
+
+dev = https://discord.com/users/698080201158033409
