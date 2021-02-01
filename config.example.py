7879d75fd023d26f2f1582cf2279b9b715b545ed
Bluewy <bluewyechache@gmail.com>
2021-02-01 19:56:02 +0100

    Update config.example.py

--- config.example.py
+++ config.example.py
@@ -4,9 +4,9 @@ extensions = ['info.py',
               'jishaku'
 ]
 
-red = (7f0000)
-green = (2aa72a)
-orange = (FF7F50)
-blue = (00BFFF)
+red = (0x7f0000)
+green = (0x2aa72a)
+orange = (0xFF7F50)
+blue = (0x00BFFF)
 
 dev = https://discord.com/users/698080201158033409
