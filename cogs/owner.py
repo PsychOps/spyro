e37a55b402a6bd78cd9b1f7e06f2b286c1199193
Bluewy <bluewyechache@gmail.com>
2021-02-08 21:55:34 +0100

    Update owner.py

--- cogs/owner.py
+++ cogs/owner.py
@@ -9,6 +9,9 @@ class owner(commands.Cog, name="Owner"):
     @commands.command(brief="unload a cog")
     @commands.is_owner()
     async def unload(self, ctx, *, cog):
+        if cog == cogs.owner:
+            await ctx.send('**You cannot unload the owner cog as this cog allows unloading/reloading/loading cogs.**')
+            return
         try:
             self.bot.unload_extension(cog)
             await ctx.send(f'Successfully unloaded {cog}')
