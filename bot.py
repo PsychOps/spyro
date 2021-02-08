184f94fcf63996963fcda9801b808328c61e6c81
Duck <duckmaster.dev@gmail.com>
2021-02-08 15:09:42 -0500

    Cleaned up the file :sponge:

--- bot.py
+++ bot.py
@@ -5,25 +5,24 @@ import psutil
 import traceback
 import logging
 from discord.ext import commands
-
-"""logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-logger.addHandler(handler)"""# Make it a little simpler to just log it to console for right now since everything is breaking e.e
 logging.basicConfig(level=logging.INFO)
 
-#  bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, allowed_mentions=discord.AllowedMentions(roles=False, users=False, everyone=False))
+bot = commands.Bot(
+    command_prefix=commands.when_mentioned_or('sp!'),
+    case_insensitive=True,
+    allowed_mentions=discord.AllowedMentions.none(),
+    max_messages=10000,
+    intents=discord.Intents.default(),
+    owner_ids=[698080201158033409, 443217277580738571],
+    status=discord.Status.online,
+    activity=discord.Activity(type=discord.ActivityType.playing, name='sp!help'),
+    description="A very cool discord bot for your server!"
+)
 
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('sp!'), case_insensitive=True, allowed_mentions=discord.AllowedMentions.none(), max_messages=10000, intents=discord.Intents.default())
-
-@bot.event  # sets the bot status and prints when it has started in console with stats, stats include: The amount of users that are in the total amount of guilds and the discord.py version
+@bot.event
 async def on_ready():
-    activity = discord.Game(name='sp!help', type=1)
-    await bot.change_presence(status=discord.Status.online, activity=activity)
-    print('\n-= spyro has started successfully =-\n')
+    print(f'\n-= {bot.name} has started successfully =-\n')
 
-print('Test 2 works')
 for extension in config.extensions:
     try:
         bot.load_extension(extension)
@@ -32,7 +31,5 @@ for extension in config.extensions:
         tb = traceback.format_exception(type(e), e, e.__traceback__)
         tbe = "".join(tb) + ""
         print(f'[WARNING] Could not load extension {extension}: {tbe}')
-
-print('Test 3 works')
 
 bot.run(config.token)
