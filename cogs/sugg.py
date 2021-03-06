18906ca6cf02a0dceb3e8368ed96094a6dde8385
Joshua <bluewyechache@gmail.com>
2021-03-06 12:50:00 +0100

    Update sugg.py

--- cogs/sugg.py
+++ cogs/sugg.py
@@ -10,6 +10,9 @@ class sugg(commands.Cog, name="Suggestions"):
         channel = self.bot.get_channel(817714676934377504)
         if message.channel != channel:
             return
+        user = self.bot.fetch_user(805872242184683551)
+        if message.author == user:
+            return
         await message.delete()
         await channel.send(message.content)
 
