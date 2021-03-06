c8a639e223ec768b307a596e90c34d90a9798aa8
Joshua <bluewyechache@gmail.com>
2021-03-06 13:03:29 +0100

    Update sugg.py

--- cogs/sugg.py
+++ cogs/sugg.py
@@ -13,8 +13,8 @@ class sugg(commands.Cog, name="Suggestions"):
         if message.author == self.bot.user:
             return
         await message.delete()
-        await channel.send(message.content)
-        await add_reaction(message, '👍')
+        om = await channel.send(message.content)
+        await om.add_reaction(['👍', '👎', '🤷'])
 
 def setup(bot):
     bot.add_cog(sugg(bot))
