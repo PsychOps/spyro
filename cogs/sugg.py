b617dc6f4aed622fbd8dc46ae1f20a94a4238606
Joshua <bluewyechache@gmail.com>
2021-03-06 12:25:21 +0100

    Update sugg.py

--- cogs/sugg.py
+++ cogs/sugg.py
@@ -8,9 +8,9 @@ class sugg(commands.Cog, name="Suggestions"):
     @commands.Cog.listener()
     async def on_message(self, ctx, *, args):
         try:
-        self.bot.get_channel(817714676934377504)
-        await ctx.message.delete()
-        await ctx.send("test")
+            self.bot.get_channel(817714676934377504)
+            await ctx.message.delete()
+            await ctx.send("test")
 
 def setup(bot):
     bot.add_cog(sugg(bot))
