6189718722771dbd60b11d0031ed8fd585ad05ec
Joshua <bluewyechache@gmail.com>
2021-03-20 19:31:18 +0100

    Update custom.py

--- cogs/custom.py
+++ cogs/custom.py
@@ -35,10 +35,10 @@ class custom(commands.Cog, name="Custom"):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-            print('member join test1')
-            channel = self.bot.get_channel(715969701771083820)
-            await member.send("<@&822886791312703518> be sure to welcome our new member!")
-            print('member join test2')
+        print('member join test1')
+        channel = self.bot.get_channel(715969701771083820)
+        await member.send("<@&822886791312703518> be sure to welcome our new member!")
+        print('member join test2')
             
         
         
