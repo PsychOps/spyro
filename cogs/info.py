1abf1d206979c0cface33f891d9fe0fd4fb175e9
Joshua <bluewyechache@gmail.com>
2021-04-04 19:35:20 +0200

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -22,7 +22,7 @@ class info(commands.Cog, name="Info"):
     async def stafftest(self, ctx, *, permission):
         the_list = []
         for user in ctx.guild.members:
-            if ctx.channel.permissions_for(user).permission:
+            if ctx.channel.permissions_for(user).{permission}:
                 the_list.append(str(user))
         e = discord.Embed(color=discord.Color.blue())
         e.description = ', '.join(the_list)
